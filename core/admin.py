417844146c498d6f772f9f03022493f30dba4f62
Jesse Silverberg <jessesilverberg11@gmail.com>
2020-02-10 11:47:07 -0500

    oops

--- core/admin.py
+++ core/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Request, Donation
+from .models import Request, Donation, ClosedRequest
 
 admin.site.register(Request)
 admin.site.register(Donation)
+admin.site.register(ClosedRequest)
